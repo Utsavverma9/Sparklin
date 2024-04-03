0e664c24c82930666f91d2f4cb0e30131f16a7e3
Utsav <113773693+Utsavverma9@users.noreply.github.com>
2024-04-03 12:50:03 +0530

    Update tips.py

--- core/tips.py
+++ core/tips.py
@@ -8,16 +8,16 @@ TIPS = [
     "You like waifus? Try `waifu` command. It's not listed in `help` command.",
     "Clean bot messages by `cleanup` command.",
     "I love you.",
-    "Parrot bot has it's own documentation. <https://github.com/rtk-rnjn/Parrot/wiki/Getting-Started>",
-    "BOT IS OPEN SOURCE! <https://github.com/rtk-rnjn/Parrot>",
+    "Sparklin bot has it's own documentation. <https://github.com/rtk-rnjn/Parrot/wiki/Getting-Started>",
+    "BOT IS OPEN SOURCE! <https://github.com/Utsavverma9/Sparklin.git>",
     "If you forget what's the prefix, just mention the bot.",
     "Bot is still in development. If you find any bugs, please report it.",
     "There exists a support server for this bot. <https://discord.gg/JUcdP3ShUDR>. You can also report bugs there.",
-    "Parrot's privacy policy: <https://github.com/rtk-rnjn/Parrot/blob/main/privacy%20policy.md>",
-    "This underrated bot is owned by ritik.ranjan, want to buy it?",
+    "Sparklin's privacy policy: <https://github.com/Utsavverma9/Sparklin/blob/749140be7e05c547cdec1b73546a232c14287c40/PRIVACY_POLICY.md>",
+    "This underrated bot is owned by Utsav.verma, want to buy it?",
     "You can suggest new features for this bot in the support server. The bot developer will give you credit if your idea is implemented. ^.^",
     "This bot is so so customizable, that you can create your own custom-commands using autoresponders, all you need to know is Jinja2 templating language. <https://jinja.palletsprojects.com/en/3.0.x/templates/>",
-    "Once upon a time, there was a bot named Parrot. It was a very good bot. It was owned by ritik.ranjan. The end.",
+    "Once upon a time, there was a bot named Sparklin. It was a very good bot. It was owned by utsav.verma. The end.",
     "1729 is a Ramanujan number. It is the smallest number expressible as the sum of two cubes in two different ways. And it is also bot's lucky number.",
     "I think you are pretty",
     "You are a good person",
@@ -30,6 +30,6 @@ TIPS = [
 ]
 
 FAQS = {
-    "how to change prefix": "Use `config prefix` command. Example: `@Parrot config prefix !`",
-    "how to change parrot bot prefix": "Use `config prefix` command. Example: `@Parrot config prefix !`",
+    "how to change prefix": "Use `config prefix` command. Example: `@Sparklin config prefix !`",
+    "how to change Sparklin bot prefix": "Use `config prefix` command. Example: `@Sparklin config prefix !`",
 }
