60295293bf97a3598116156d8e68310566bf1ddc
Utsav <113773693+Utsavverma9@users.noreply.github.com>
2024-04-03 12:17:09 +0530

    Update __init__.py

--- discord/ext/old_menus/__init__.py
+++ discord/ext/old_menus/__init__.py
@@ -22,22 +22,22 @@ class MenuError(Exception):
 
 class CannotEmbedLinks(MenuError):
     def __init__(self) -> None:
-        super().__init__("Bot does not have embed links permission in this channel.")
+        super().__init__("Sparklin Bot does not have embed links permission in this channel.")
 
 
 class CannotSendMessages(MenuError):
     def __init__(self) -> None:
-        super().__init__("Bot cannot send messages in this channel.")
+        super().__init__("Sparklin Bot cannot send messages in this channel.")
 
 
 class CannotAddReactions(MenuError):
     def __init__(self) -> None:
-        super().__init__("Bot cannot add reactions in this channel.")
+        super().__init__("Sparklin Bot cannot add reactions in this channel.")
 
 
 class CannotReadMessageHistory(MenuError):
     def __init__(self) -> None:
-        super().__init__("Bot does not have Read Message History permissions in this channel.")
+        super().__init__("Sparklin Bot does not have Read Message History permissions in this channel.")
 
 
 class Position:
